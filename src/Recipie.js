5276e26d63a294b37f30dad93e019d2409a39e98
vijayabharathi <vijayabharathib@users.noreply.github.com>
2016-12-17 08:13:07 +0530

    delete ingredient

--- src/Recipie.js
+++ src/Recipie.js
@@ -8,12 +8,21 @@ class Recipie extends Component{
       {key: 2, name: "water"}
     ] };
   }
+  addIngredient(){
 
+  }
+  deleteIngredient(deleteKey){
+    let newIngredients=this.state.ingredients;
+    newIngredients=this.state.ingredients.filter(function(ingredient){
+      return ingredient.key!==deleteKey;
+    });
+    this.setState({ingredients: newIngredients});
+  }
   render(){
     return(
       <li className="c-recipie">
         <h4 className="c-recipie__name">{this.props.name}</h4>
-        <IngredientList ingredients={this.state.ingredients} />
+        <IngredientList ingredients={this.state.ingredients} onDelete={this.deleteIngredient.bind(this)} />
         <button className="c-recipie__delete" onClick={this.props.delete}>x</button>
         <button className="c-recipie__edit">Edit</button>
       </li>
