4ec03a56c06f664337023c74b8ff309d1972ba0b
vijayabharathi <vijayabharathib@users.noreply.github.com>
2016-12-17 23:52:02 +0530

    showhide ingredients

--- src/Recipie.js
+++ src/Recipie.js
@@ -3,14 +3,24 @@ import IngredientList from './Ingredients';
 class Recipie extends Component{
   constructor(){
     super();
-    this.state={ ingredients:[
-      {key: 1, name: "flour"},
-      {key: 2, name: "water"}
-    ] };
+    this.state={
+      ingredients:[
+        {key: 1, name: "flour"},
+        {key: 2, name: "water"}
+      ],
+      displayIngredient: false
+    };
   }
+
+  toggleIngredient(){
+    console.log('toggle');
+    this.setState({displayIngredient: !this.state.displayIngredient});
+  }
+
   addIngredient(){
 
   }
+
   deleteIngredient(deleteKey){
     let newIngredients=this.state.ingredients;
     newIngredients=this.state.ingredients.filter(function(ingredient){
@@ -19,10 +29,14 @@ class Recipie extends Component{
     this.setState({ingredients: newIngredients});
   }
   render(){
+    let ingList=null;
+    if(this.state.displayIngredient){
+      ingList=<IngredientList ingredients={this.state.ingredients} onDelete={this.deleteIngredient.bind(this)} />;
+    }
     return(
       <li className="c-recipie">
-        <h4 className="c-recipie__name">{this.props.name}</h4>
-        <IngredientList ingredients={this.state.ingredients} onDelete={this.deleteIngredient.bind(this)} />
+        <h4 className="c-recipie__name" onClick={this.toggleIngredient.bind(this)}>{this.props.name}</h4>
+        {ingList}
         <button className="c-recipie__delete" onClick={this.props.delete}>x</button>
         <button className="c-recipie__edit">Edit</button>
       </li>
