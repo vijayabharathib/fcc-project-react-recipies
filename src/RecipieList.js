5f64d5d190fb64497d483fc26f659a6f04aa1241
vijayabharathi <vijayabharathib@users.noreply.github.com>
2016-12-24 14:17:23 +0530

    coverall setup

--- src/RecipieList.js
+++ src/RecipieList.js
@@ -6,12 +6,6 @@ class RecipieList extends Component{
     let recipies=this.props.recipies.map(function(recipie){
       return (
         <Recipie key={recipie.key} name={recipie.name} delete={deleteItem.bind(null,recipie.key)}/>
-        /*<li className="c-recipie" key={recipie.key}>
-          <h4 className="c-recipie__name">{recipie.name}</h4>
-          <IngredientList ingredients={this.state.ingredients} />
-          <button className="c-recipie__delete" onClick={deleteItem.bind(null,recipie.key)}>x</button>
-          <button className="c-recipie__edit">Edit</button>
-        </li>*/
       )
     })
 
